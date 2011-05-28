6543a27102dbc73fe2b4dd340654da6a30179e27
Logan Smyth <loganfsmyth@gmail.com>
2011-05-28 13:09:01 -0400

    Clean up foursquare coffeescript file to remember how coffeescript works, even if we aren't going to use Foursquare anymore

--- app/coffeescripts/foursquare.coffee
+++ app/coffeescripts/foursquare.coffee
@@ -1,70 +1,40 @@
 client_id = 'I1ATRYBZLDI0EDEZFGC1BADYRVRMLOUMB3GNNS2WNB1U4LHE'
 secret = 'BCZOYVOBG2GOBTR2NTSNX3QVAVWDDJN2AZ2XIIEPGJQRV1TK'
 search_url = 'https://api.foursquare.com/v2/venues/search'
-default_params = {
+default_params =
   client_id: client_id,
   client_secret: secret
-}
+
+types =
+  GroceryStores:      '4bf58dd8d48988d118951735',
+  BusStops:           '4bf58dd8d48988d1fe931735',
+  MetroStops:         '4bf58dd8d48988d1fd931735',
+  Hospitals:          '4bf58dd8d48988d196941735',
+  Gyms:               '4bf58dd8d48988d196941735',
+  Police:             '4bf58dd8d48988d12e941735',
+  Fire:               '4bf58dd8d48988d12c941735',
+  Parks:              '4bf58dd8d48988d163941735',
+  Schools:            '4bf58dd8d48988d13b941735',
+  ConvenienceStores:  '4d954b0ea243a5684a65b473',
+  Drugstores:         '4bf58dd8d48988d10f951735',
 
 getVenuesNear = (lat, lng, callback, extra_args) ->
   latlng = lat + ',' + lng
-  params = $.extend({}, default_params, {ll: latlng})
-  if (extra_args) 
-    params = $.extend(params, extra_args)
+  params = $.extend {}, default_params, ll: latlng
+  if extra_args
+    params = $.extend params, extra_args
 
-  $.getJSON(search_url, params, (json) ->
+  $.getJSON search_url, params, (json) ->
     items = json.response.groups[0].items
-    items = items.sort((el1, el2) -> 
+    items = items.sort (el1, el2) ->
       el1.location.distance - el2.location.distance
-    )
-    callback(items)
-  )
 
-@foursquare = 
+    callback items
 
+@foursquare =
   getVenuesNear: getVenuesNear
 
-  getGroceryStoresNear: (lat, lng, callback) ->
-    category_id = '4bf58dd8d48988d118951735'
-    getVenuesNear(lat, lng, callback, {categoryId: category_id})
-
-  getBusStopsNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d1fe931735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-   
-  getMetroStopsNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d1fd931735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-
-  getHospitalsNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d196941735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-   
-  getGymsNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d176941735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-   
-  getPoliceNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d12e941735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-   
-  getFireNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d12c941735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-
-  getParksNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d163941735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-
-  getSchoolsNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d13b941735'
-    getVenuesNear(lat, lng, cb,  {categoryId: cat })
-
-  getConvenienceStoresNear: (lat, lng, cb) ->
-    cat = '4d954b0ea243a5684a65b473'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-
-  getDrugstoresNear: (lat, lng, cb) ->
-    cat = '4bf58dd8d48988d10f951735'
-    getVenuesNear(lat, lng, cb, {categoryId: cat})
-
+for own name,cat of types
+  ((cat) ->
+    @foursquare['get' + name + 'Near'] = (lat, lng, cb) -> getVenuesNear lat, lng, cb, categoryId: cat
+  )(cat)
--- public/javascripts/foursquare.js
+++ public/javascripts/foursquare.js
@@ -1,15 +1,29 @@
-/* DO NOT MODIFY. This file was compiled Sun, 22 May 2011 16:55:04 GMT from
- * /Users/simon/Documents/startupifier/mynextpad/app/coffeescripts/foursquare.coffee
+/* DO NOT MODIFY. This file was compiled Sat, 28 May 2011 17:06:40 GMT from
+ * /Users/logansmyth/projects/mynextpad2/app/coffeescripts/foursquare.coffee
  */
 
 (function() {
-  var client_id, default_params, getVenuesNear, search_url, secret;
+  var cat, client_id, default_params, getVenuesNear, name, search_url, secret, types, _fn;
+  var __hasProp = Object.prototype.hasOwnProperty;
   client_id = 'I1ATRYBZLDI0EDEZFGC1BADYRVRMLOUMB3GNNS2WNB1U4LHE';
   secret = 'BCZOYVOBG2GOBTR2NTSNX3QVAVWDDJN2AZ2XIIEPGJQRV1TK';
   search_url = 'https://api.foursquare.com/v2/venues/search';
   default_params = {
     client_id: client_id,
     client_secret: secret
+  };
+  types = {
+    GroceryStores: '4bf58dd8d48988d118951735',
+    BusStops: '4bf58dd8d48988d1fe931735',
+    MetroStops: '4bf58dd8d48988d1fd931735',
+    Hospitals: '4bf58dd8d48988d196941735',
+    Gyms: '4bf58dd8d48988d196941735',
+    Police: '4bf58dd8d48988d12e941735',
+    Fire: '4bf58dd8d48988d12c941735',
+    Parks: '4bf58dd8d48988d163941735',
+    Schools: '4bf58dd8d48988d13b941735',
+    ConvenienceStores: '4d954b0ea243a5684a65b473',
+    Drugstores: '4bf58dd8d48988d10f951735'
   };
   getVenuesNear = function(lat, lng, callback, extra_args) {
     var latlng, params;
@@ -30,83 +44,18 @@
     });
   };
   this.foursquare = {
-    getVenuesNear: getVenuesNear,
-    getGroceryStoresNear: function(lat, lng, callback) {
-      var category_id;
-      category_id = '4bf58dd8d48988d118951735';
-      return getVenuesNear(lat, lng, callback, {
-        categoryId: category_id
-      });
-    },
-    getBusStopsNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d1fe931735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getMetroStopsNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d1fd931735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getHospitalsNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d196941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getGymsNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d176941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getPoliceNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d12e941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getFireNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d12c941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getParksNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d163941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getSchoolsNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d13b941735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getConvenienceStoresNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4d954b0ea243a5684a65b473';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    },
-    getDrugstoresNear: function(lat, lng, cb) {
-      var cat;
-      cat = '4bf58dd8d48988d10f951735';
-      return getVenuesNear(lat, lng, cb, {
-        categoryId: cat
-      });
-    }
+    getVenuesNear: getVenuesNear
   };
+  _fn = function(cat) {
+    return this.foursquare['get' + name + 'Near'] = function(lat, lng, cb) {
+      return getVenuesNear(lat, lng, cb, {
+        categoryId: cat
+      });
+    };
+  };
+  for (name in types) {
+    if (!__hasProp.call(types, name)) continue;
+    cat = types[name];
+    _fn(cat);
+  }
 }).call(this);
